dec0907100346f78a86de355c0f6ba0e8b0eba16
eduardomelonascimento <nascimentomeloedu@gmail.com>
2022-03-20 22:38:54 -0300

    777

---
+++ exercícios/estudos-css/menu-responsivo/menu.js
@@ -0,0 +1,15 @@
+(function menuToggle() {
+
+    var $toggle = document.querySelector('#button');
+    var $menu = document.querySelector('.navbar-items')
+    var $lines = document.querySelectorAll('.line');
+
+    $toggle.addEventListener('click', function () {
+        for (var i = 0; $lines[i]; i++) {
+            $lines[i].classList.toggle(`line${i+1}-close`)
+        }
+        $menu.classList.toggle('navbar-items');
+        $menu.classList.toggle('navbar-items-open');
+    })
+
+})()
