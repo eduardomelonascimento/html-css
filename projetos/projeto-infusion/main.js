4b68f4317ae567459397bcad8fec701bec34ddcd
eduardomelonascimento <nascimentomeloedu@gmail.com>
2022-03-24 12:17:46 -0300

    777

--- projetos/projeto-infusion/main.js
+++ projetos/projeto-infusion/main.js
@@ -1,4 +1,29 @@
 (function () {
 
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js');
+    $body.classList.add('js');
+
     
+
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__menuToggle',
+        widthEnabled: 1000
+    });
+
+    var carouselNote = new Carousel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+    var carouselQuotes = new Carousel({
+        container: '.quote-container .quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
 })()
